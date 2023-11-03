5a2fd3548a8b677646f2f59af6964383aac5b4ca
1urvashi <urvashidhokiya8@gmail.com>
2023-11-03 19:23:53 +0530

    PHPDocumentor style code generate

--- task2.php
+++ task2.php
@@ -1,2 +1,62 @@
-<?
-// Your code is here:
+<?php
+// Create a connection to your database.
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "nota_test";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
+
+// URL of the Wikipedia page to download
+$url = "https://www.wikipedia.org/";
+
+// Download the webpage
+$html = file_get_contents($url);
+
+// Parse the HTML using DOMDocument
+$doc = new DOMDocument();
+libxml_use_internal_errors(true); 
+$doc->loadHTML($html);
+
+// Find and process the sections of the page
+$sections = $doc->getElementsByTagName('section');
+foreach ($sections as $section) {
+
+    // Extract and sanitize the title
+    $title = $section->getElementsByTagName('h2')->item(0)->textContent;
+    $title = substr($title, 0, 230); // Truncate to a maximum of 230 characters
+    $title = htmlspecialchars($title, ENT_QUOTES);
+
+    // Extract and sanitize the abstract
+    $abstract = $section->getElementsByTagName('p')->item(0)->textContent;
+    $abstract = substr($abstract, 0, 256); // Truncate to a maximum of 256 characters
+    $abstract = htmlspecialchars($abstract, ENT_QUOTES);
+
+    // Extract and sanitize the picture URL
+    $picture = $section->getElementsByTagName('img')->item(0)->getAttribute('src');
+    $picture = substr($picture, 0, 240); // Truncate to a maximum of 240 characters
+    $picture = htmlspecialchars($picture, ENT_QUOTES);
+
+    // Extract the link
+    $link = $section->getElementsByTagName('a')->item(0)->getAttribute('href');
+
+    // Insert data into the database
+    $stmt = $conn->prepare("INSERT INTO wiki_sections (date_created, title, url, picture, abstract) 
+                            VALUES (NOW(), :title, :url, :picture, :abstract)");
+    $stmt->bindParam(':title', $title);
+    $stmt->bindParam(':url', $url);
+    $stmt->bindParam(':picture', $picture);
+    $stmt->bindParam(':abstract', $abstract);
+    $stmt->execute();
+}
+
+// Close the database connection
+$conn = null;
+
+?>
--- task3.php
+++ task3.php
@@ -1,2 +1,79 @@
-<?
-// Your code is here:
+<?php
+
+/**
+ * Class TableCreator
+ *
+ * This class is used to create a table called 'Test' and perform operations on it.
+ *
+ * @final
+ */
+final class TableCreator {
+    private $db; // Database connection object
+
+    public function __construct() {
+        
+        $this->db = new PDO("mysql:host=localhost;dbname=nota_test", "root", "root");
+
+        // Create the 'Test' table
+        $this->create();
+        // Fill the 'Test' table with random data
+        $this->fill();
+    }
+
+    /**
+     * Create the 'Test' table with specified fields.
+     *
+     * This method is accessible only within the class.
+     */
+    private function create() {
+        $query = "CREATE TABLE Test (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            script_name VARCHAR(25),
+            start_time DATETIME,
+            end_time DATETIME,
+            result ENUM('normal', 'illegal', 'failed', 'success')
+        )";
+        
+        $this->db->exec($query);
+    }
+
+    /**
+     * Fill the 'Test' table with random data.
+     *
+     * This method is accessible only within the class.
+     */
+    private function fill() {
+        
+        $stmt = $this->db->prepare("INSERT INTO Test (script_name, start_time, end_time, result) VALUES (?, ?, ?, ?)");
+
+        for ($i = 0; $i < 10; $i++) {
+            $scriptName = "Script " . rand(1, 100);
+            $startTime = date("Y-m-d H:i:s", rand(1609459200, 1709459200)); // Random date between two timestamps
+            $endTime = date("Y-m-d H:i:s", rand(1709459200, 1809459200)); // Random date between two timestamps
+            $result = ['normal', 'illegal', 'failed', 'success'][rand(0, 3)];
+
+            $stmt->execute([$scriptName, $startTime, $endTime, $result]);
+        }
+    }
+
+    /**
+     * Select data from the 'Test' table based on the 'result' column.
+     *
+     * This method is accessible from outside the class.
+     *
+     * @return array
+     */
+    public function get() {
+        $query = "SELECT * FROM Test WHERE result IN ('normal', 'success')";
+        $stmt = $this->db->query($query);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+// Example usage:
+$tableCreator = new TableCreator();
+$data = $tableCreator->get();
+print_r($data);
+
+?>
